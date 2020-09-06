050b8fddc5551f6ee1a6077f7e969952bbcb164e
Eugen Marin <eugen.marin@outlook.com>
2020-09-06 13:30:17 +0300

    Added two more functions: getInverseNumber() and getSumProdDivisors()

--- BasicCpp/main.cpp
+++ BasicCpp/main.cpp
@@ -1,15 +1,52 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 void lastDigitSquare();
+void getInverseNumber();
+void getSumProdDivisors();
 
 // ----------------------------------------
 // main(): Everything starts here :-)
 // ----------------------------------------
 int main(int argc, char* argv[])
 {
-	lastDigitSquare();
+	system("clear");
+	int op;
+
+	while (true) {
+		cout << endl << endl;
+		cout << "*****************************************" << endl;
+		cout << "Basic C++ functions" << endl;
+		cout << "*****************************************" << endl;
+		cout << "1. Get square of last digit" << endl;
+		cout << "2. Get inverse number for digits avg < 5" << endl;
+		cout << "3. Get sum and prod for divisors of a number" << endl;
+		cout << "0. EXIT" << endl;
+		cout << "*****************************************" << endl;
+		cout << "Your option: ";
+		cin >> op;
+
+		switch (op)
+		{
+		case 1:
+			lastDigitSquare();
+			break;
+		case 2:
+			getInverseNumber();
+			break;
+		case 3:
+			getSumProdDivisors();
+			break;
+		case 0:
+			return 0;
+		default:
+			break;
+		}
+
+	}
+
 	return 0;
 }
 
@@ -40,4 +77,71 @@ void lastDigitSquare()
 		cout << "There is no number with last digit == " << n;
 
 	cout << endl;
+}
+
+// ----------------------------------------
+// Reads positive integers (stops when 0).
+// For each number, gets all digits and calculates average.
+// If avg < 5, prints the inverse of that number (e.g. if number is 1034, print 4301)
+// ----------------------------------------
+void getInverseNumber()
+{
+	unsigned int number = 1;
+	unsigned int sumdigits = 0;
+	unsigned int inverse = 0;
+
+	while (number > 0) {
+		cout << "Input n: ";
+		cin >> number;
+
+		inverse = 0;
+		sumdigits = 0;
+		unsigned int x = 0;
+		unsigned int temp = number;
+		while (temp > 0) {
+			int digit = temp % 10;
+			inverse = inverse * 10 + digit;
+			sumdigits += digit;
+			temp /= 10;
+			x++;
+		}
+
+		if (x > 0) {
+			if (sumdigits / x < 5)
+				cout << "Inverse number is: " << inverse << endl;
+			else
+				cout << "The average of its digits is >= 5" << endl;
+		}
+
+	}
+}
+
+// ----------------------------------------
+// Reads an integer number and obtains its divisor.
+// Prints the sum and prod of its divisors.
+// If no divisor, prints a message.
+// ----------------------------------------
+void getSumProdDivisors()
+{
+	unsigned int n;
+	cout << "Input n: ";
+	cin >> n;
+
+	unsigned int sum = 0;
+	unsigned int prod = 1;
+
+	for (unsigned int i = 2; i <= n/2; i++) {
+		if (n % i == 0) {
+			sum += i;
+			prod *= i;
+		}
+	}
+	if (sum == 0)
+		cout << "There is no divisor for " << n << endl;
+	else
+	{
+		cout << "Sum of its divisors is: " << sum << endl;
+		cout << "Prod of its divisors is: " << prod << endl;
+	}
+	
 }
